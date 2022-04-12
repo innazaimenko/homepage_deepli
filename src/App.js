2794227d8be7331155cd6fcd9ea5e8b44869a7d9
innazaimenko <innazb@gmail.com>
2022-04-12 10:43:25 +0200

    nk

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
-        <a
+        <button onclick="myFunction()">Click me</button>
+        
+        {/* <a
+          
           className="App-link"
-          href=""
+          href="https://www.deepli.net"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
