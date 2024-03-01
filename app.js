05abb583f17880b6041fcba91d670d789d6e9093
innazaimenko <innazb@gmail.com>
2024-03-01 08:09:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var btn = document.querySelector('.modal-btn');
-var popupLoad = document.querySelector('.modal-bg');
-var modalClose = document.querySelector('.modal-close');
+//var btn = document.querySelector('.modal-btn');
+//var popupLoad = document.querySelector('.modal-bg');
+//var modalClose = document.querySelector('.modal-close');
 
-window.addEventListener('load', function () {
-    popupLoad.classList.add('popup-active');
-});
+//window.addEventListener('load', function () {
+//    popupLoad.classList.add('popup-active');
+//});
 
-modalClose.addEventListener('click', function (){
-    popupLoad.classList.remove('popup-active');
-});
+//modalClose.addEventListener('click', function (){
+//    popupLoad.classList.remove('popup-active');
+//});
 
 
 window.addEventListener('load', ()=> {
